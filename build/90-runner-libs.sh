6a36901f918e3bbf2e784ff0a6f746b5b8f581c5
David Arnold <dar@devco.co>
2017-07-02 15:58:47 -0500

    [CLEANUP] just remove ineffective docker version pin

--- build/90-runner-libs.sh
+++ build/90-runner-libs.sh
@@ -4,7 +4,7 @@ set -x \
         curl \
         git-all \
         openssh-client \
-    && curl -sSL https://get.docker.com/ | sed 's/lxc-docker/lxc-docker-1.11/' | sh > /dev/null \
+    && curl -sSL https://get.docker.com/ | sh > /dev/null \
     && curl -L "https://github.com/docker/compose/releases/download/1.14.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose  > /dev/null \
     && chmod +x /usr/local/bin/docker-compose \
     && rm -rf /var/lib/apt/lists/*
