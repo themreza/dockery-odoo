2aeb0a70de3842ce8b80dd573335d7ec946b64b9
David Arnold <dar@xoe.solutions>
2018-02-04 22:00:18 -0500

    Remove CI adaption
    Rather, CI should to adapt to the production setup.

--- entrypoint.d/10-construct-addons-folder.sh
+++ entrypoint.d/10-construct-addons-folder.sh
@@ -3,12 +3,9 @@
 
 set -e
 addonspath=""
-# If we are in a CI environment, just prefix with the environments project folder
-# This enables namespaced parallel builds
-ciprojectfolder=${CI_PROJECT_DIR}
 # Sort alfanumerically first, then do realpath so we can freely reorder loading
 # by symlinking for exemple in a CI environment directly from a git clone.
-for dir in $(find ${ciprojectfolder}/opt/odoo/addons -maxdepth 1 -mindepth 1 -xtype d | sort | xargs realpath) $(realpath ${ciprojectfolder}/opt/odoo/odoo/addons); do
+for dir in $(find /opt/odoo/addons -maxdepth 1 -mindepth 1 -xtype d | sort | xargs realpath) $(realpath /opt/odoo/odoo/addons); do
     echo "Adding $dir to addons path"
     if [ -z "$addonspath" ]; then
         addonspath=$dir
