5bfe8011f05a0c4c57a0bfef2a3bcd5f34deeb4d
David Arnold <dar@xoe.solutions>
2018-02-06 18:50:40 -0500

    [FIX] effectively sources files in build script

--- bin/build-dir-exec.sh
+++ bin/build-dir-exec.sh
@@ -4,5 +4,5 @@ set -e
 folder=$(basename $0 .sh)
 for file in $(find /$folder.d -maxdepth 1 -mindepth 1 -xtype f -exec realpath {} + | sort); do
     echo Sourcing "$file" > /dev/stderr
-    exec $file
+    source $file
 done
