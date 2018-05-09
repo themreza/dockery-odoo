a6c6eb31ab304cd6b43c0f315d5016a9434408b2
David Arnold <dar@xoe.solutions>
2018-05-08 20:19:24 -0500

    [IMP] enforce folder structure

--- base/entrypoint.d/10-construct-addons-folder.sh
+++ base/entrypoint.d/10-construct-addons-folder.sh
@@ -3,9 +3,10 @@
 
 set -e
 addonspath=""
-# Sort alfanumerically first, then do realpath so we can freely reorder loading
-# by symlinking for exemple in a CI environment directly from a git clone.
-for dir in $(find /opt/odoo/addons -maxdepth 1 -mindepth 1 -xtype d | sort | xargs realpath) $(realpath /opt/odoo/odoo/addons); do
+# Sort reverse alfanumerically first, then do realpath 
+# so we can freely reorder loading by symlinking for
+# exemple in a CI environment directly from a git clone.
+for dir in $(find "${ADDONS_BASE}" -maxdepth 1 -mindepth 1 -xtype d | sort -r | xargs realpath); do
     echo "Adding $dir to addons path"
     if [ -z "$addonspath" ]; then
         addonspath=$dir
