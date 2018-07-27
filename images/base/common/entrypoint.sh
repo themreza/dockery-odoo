d2b6990d9bed21f8a769525655107d8ba48da589
David Arnold <dar@xoe.solutions>
2018-07-27 16:53:58 -0500

    [FIX] pacthes application

--- images/base/common/entrypoint.sh
+++ images/base/common/entrypoint.sh
@@ -11,55 +11,72 @@ set -Eeuxo pipefail
 # It is meant to be adapted or extended in downstream containers.
 # -----------------------------------------------------------------------------
 
+# Implemented command options
+if [ "$#" -eq 0 ] || [ "${1:0:1}" = '-' ]; then
+	set -- run "$@"
+fi
+
+CMD=( "$@" )
+
+if [ "$1" = 'apply-patches' ]; then
+	# additional arguments will be passed to patch
+	# Bind mount (writable) you odoo folder
+	# while appling those patches
+	CMD=(
+			"apply-patches"
+			"--quiet"
+			"${CMD[@]:1}"
+		)
+fi
+
 # shellcheck disable=SC1091
 source /entrypoint.appenv.sh
 # shellcheck disable=SC1091
 source /entrypoint.sourced.sh
 set +x
 
-# Implemented command options
-if [ "$#" -eq 0 ] || [ "${1:0:1}" = '-' ]; then
-	set -- run "$@"
-fi
 
 
-CMD=( "$@" )
 
 set +u
 
 if [ "$1" = 'run' ]; then
 	sourceScriptsInFolder "/entrypoint.d"
-	CMD=("${ODOO_CMD}"
-		"--addons-path"
-		"${ODOO_ADDONSPATH}"
-		"${CMD[@]:1}")
+	CMD=(
+			"${ODOO_CMD}"
+			"--addons-path"
+			"${ODOO_ADDONSPATH}"
+			"${CMD[@]:1}"
+		)
 fi
 
 if [ "$1" = 'shell' ]; then
 	database="$1"
 	sourceScriptsInFolder "/entrypoint.d"
-	CMD=("${ODOO_CMD}"
-		"shell"
-		"--addons-path"
-		"${ODOO_ADDONSPATH}"
-		"-d" "${database}"
-		"${CMD[@]:1}")
+	CMD=(
+			"${ODOO_CMD}"
+			"shell"
+			"--addons-path"
+			"${ODOO_ADDONSPATH}"
+			"-d" "${database}"
+			"${CMD[@]:1}"
+		)
 fi
 
 if [ "$1" = 'scaffold' ]; then
-	CMD=("${ODOO_CMD} ${CMD[@]}")
+	CMD=(
+			"${ODOO_CMD}"
+			"${CMD[@]}"
+		)
 fi
 
 if [ "$1" = 'deploy' ]; then
-	CMD=("${ODOO_CMD} ${CMD[@]}")
+	CMD=(
+			"${ODOO_CMD}"
+			"${CMD[@]}"
+		)
 fi
 
-if [ "$1" = 'apply-patches' ]; then
-	# additional arguments will be passed to patch
-	# Bind mount (writable) you odoo folder
-	# while appling those patches
-	CMD=("apply-patches --quiet ${CMD[@]:1}")
-fi
 set -u
 
 set -x
