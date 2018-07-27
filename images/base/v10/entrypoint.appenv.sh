2865186fcb82567cb3d3bd2864b3dd5f8f47d6d4
David Arnold <dar@xoe.solutions>
2018-07-27 06:44:06 -0500

    [FIX] env variable

--- images/base/v10/entrypoint.appenv.sh
+++ images/base/v10/entrypoint.appenv.sh
@@ -20,7 +20,7 @@ addonspath=""
 # Sort reverse alfanumerically first, then do realpath 
 # so we can freely reorder loading by symlinking for
 # exemple in a CI environment directly from a git clone.
-for dir in $(find "${ADDONS_BASE}" -maxdepth 1 -mindepth 1 -xtype d | sort -r | xargs realpath); do
+for dir in $(find "${ODOO_ADDONS_BASEPATH}" -maxdepth 1 -mindepth 1 -xtype d | sort -r | xargs realpath); do
     echo "Adding $dir to addons path"
     if [ -z "$addonspath" ]; then
         addonspath=$dir
