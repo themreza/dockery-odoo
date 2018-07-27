3e03ff6f95ec9a0a0fc3ab4bfef6568ecb6ec6f0
David Arnold <dar@xoe.solutions>
2018-07-27 06:44:18 -0500

    [IMP] script verbosity

--- images/base/gen_images.sh
+++ images/base/gen_images.sh
@@ -6,12 +6,23 @@
 
 set -Eeuxo pipefail
 
+
+set +u
+if [ -z "${1+x}" ]; then
+	echo -e "Usage: gen_images IMAGEREPO"
+	echo -e ""
+	echo -e "Use IMAGEREPO in the form repo/image without tags."
+	echo -e "Tags will be created by the scripts as per app version."
+fi
+set -u
+
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 
 source "${DIR}"/gen_contexts.sh
 
 for version in $(find "${DIR}" -maxdepth 1 -type d -name 'v*'); do
-	name=$(basename ${version})
+	name=$(basename "${version}")
 	docker build --tag "${1}:${name}" "${version}"
 done
