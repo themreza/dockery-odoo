f788dd8bc27ef250e6a8b4dd9206f0ee277c5577
David Arnold <dar@xoe.solutions>
2018-08-16 20:53:26 -0500

    [IMP] add enterprise flag

--- images/base/common/entrypoint.appenv.sh
+++ images/base/common/entrypoint.appenv.sh
@@ -21,6 +21,12 @@ addonspath=""
 # so we can freely reorder loading by symlinking for
 # exemple in a CI environment directly from a git clone.
 for dir in $(find "${ODOO_ADDONS_BASEPATH}" -maxdepth 1 -mindepth 1 -xtype d | sort -r | xargs realpath); do
+
+	# Prevent loading enterprise addons folder if switched on
+	if [[ $ODOO_ENTERPRISE != 'true' ]] && [[ $dir == "${ODOO_ADDONS_BASEPATH}"/001 ]]; then
+		continue
+	fi
+
     echo "Adding $dir to addons path"
     if [ -z "$addonspath" ]; then
         addonspath=$dir
