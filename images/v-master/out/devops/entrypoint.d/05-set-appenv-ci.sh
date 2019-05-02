de159c909f30cbba1cd63b99d31502451bdf82b6
David Arnold <dar@xoe.solutions>
2019-05-02 12:28:34 -0500

    [FIX] add PYTHONPATH on Ci environment

--- images/v-master/out/devops/entrypoint.d/05-set-appenv-ci.sh
+++ images/v-master/out/devops/entrypoint.d/05-set-appenv-ci.sh
@@ -14,4 +14,5 @@ if [ -z "${CI+x}" ]; then
 	export ODOO_VENDOR="${ODOO_BASEPATH}/vendor"
 	export ODOO_SRC="${ODOO_BASEPATH}/src"
 	export PATCHES_DIR="${ODOO_BASEPATH}/patches.d"
+	export PYTHONPATH="${PYTHONPATH}:${ODOO_FRM}"
 fi
