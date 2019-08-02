8f72deca518e2c86dc68d494aefa2881025ded7f
David Arnold <dar@xoe.solutions>
2019-08-02 14:27:17 +0200

    [IMP] shorten welcome message for devops

--- images/v-master/out/devops/entrypoint.d/25-prelude-devops.sh
+++ images/v-master/out/devops/entrypoint.d/25-prelude-devops.sh
@@ -5,23 +5,9 @@ set -Eeuo pipefail
 
 
 echo -en """
-
-${LGREEN}You're running the ${LYELLOW}:devops${LGREEN} multi-talent. It's an enhanced ${LYELLOW}:base${LGREEN} and vital${NC}
-${LGREEN}component of Dockery-Odoo's powerful ${LYELLOW}twin-image design pattern${LGREEN}.${NC}
-
-${LGREEN}Use it for:${NC}
-
- ${LCYAN}Debugging, Scaffolding, Testing, Migrating, DB Seeding, Loading (ETL)${NC}
-
-${LGREEN}It incorporates the excellent dodoo-* toolchain to provide native${NC}
-${LGREEN}Odoo Server API extension. More info: https://git.io/fpJLp${NC}
-
-${LGREEN}=== contributions welcome ===${NC}
-
-
-${LGREEN}================================================================================${NC}
-${LGREEN}================================================================================${NC}
-
-
-
+${LGREEN}Welcome to the ${LYELLOW}devops${LGREEN} image.${NC}
+${LGREEN}It's your dev companion of the ${LYELLOW}base${LGREEN} image.${NC}
+${LGREEN}===============================================================================${NC}
+${LGREEN}===                          ${LYELLOW}contributions welcome${LGREEN}                          ===${NC}
+${LGREEN}===============================================================================${NC}
 """
