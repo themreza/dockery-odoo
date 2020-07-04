2ec8e353e915e7c8dccacb498ec8bc859c51077d
Yves Goldberg <admin@ygol.com>
2020-07-04 13:52:14 +0300

    Update 05-set-appenv-ci.sh

--- _spec-common-devops-additions/entrypoint.d/05-set-appenv-ci.sh
+++ _spec-common-devops-additions/entrypoint.d/05-set-appenv-ci.sh
@@ -7,10 +7,10 @@ set -Eeuo pipefail
 
 if [ ! -z "${CI+x}" ]; then
     echo "==>  Setting up paths in CI env"
-	export ODOO_RC="${ODOO_BASEPATH}/cfg.d"
+	export ODOO_RC="${ODOO_BASEPATH}/odoo.conf"
 	export ODOO_MIG="${ODOO_BASEPATH}/migration.yaml"
-	export ODOO_CMD="${ODOO_BASEPATH}/vendor/odoo/cc/odoo-bin"
-	export ODOO_FRM="${ODOO_BASEPATH}/vendor/odoo/cc"
+	export ODOO_CMD="${ODOO_BASEPATH}/odoo/odoo-bin"
+	export ODOO_FRM="${ODOO_BASEPATH}/odoo"
 	export ODOO_VENDOR="${ODOO_BASEPATH}/vendor"
 	export ODOO_SRC="${ODOO_BASEPATH}/src"
 	export PATCHES_DIR="${ODOO_BASEPATH}/patches.d"
